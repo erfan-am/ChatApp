d8da6da277bce0109f6119e89d8ab2c640766819
erfan-am <erfanam582@gmail.com>
2020-11-16 13:32:21 +0330

    React FormValidation

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 const Input = (props) => {
+    const element=props.text ==="text" ?
+    <textarea/>
+    :
+    <input
+    style={{border:props.valid && props.end ? '1px solid green' : '1px solid #333'}}
+    placeholder={props.type !=="checkbox" ? props.placeholder : undefined}
+    value={props.type !=="checkbox"  && props.value}
+    className={`${props.type === "checkbox" && 'checkbox'}`}
+    name={props.name}
+    type={props.type}
+    onClick={props.onCheck }
+    onChange={props.type !=="checkbox"  ? props.onChange : undefined}
+    onBlur={props.type !=="checkbox" ? props.touchInput : undefined}
+    required
+/> 
     return (
     <div className="input_group">
-        <input
-            style={{border:props.valid && props.end ? '1px solid green' : '1px solid #333'}}
-            placeholder={props.type !=="checkbox" ? props.placeholder : undefined}
-            value={props.type !=="checkbox"  && props.value}
-            className={`${props.type === "checkbox" && 'checkbox'}`}
-            name={props.name}
-            type={props.type}
-            onClick={props.onCheck }
-            onChange={props.type !=="checkbox"  ? props.onChange : undefined}
-            onBlur={props.type !=="checkbox" ? props.touchInput : undefined}
-            required
-        /> 
+      {element}
         <label style={{
                 color:!props.valid ? 
                 props.touched && 'red' : 
